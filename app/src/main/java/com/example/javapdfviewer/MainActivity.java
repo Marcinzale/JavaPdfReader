1a9998b118ccc5e224f427df34b4495986a91a3e
Marcin <marcinzale@gmail.com>
2023-02-01 18:46:26 +0100

    First commit

---
+++ app/src/main/java/com/example/javapdfviewer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.javapdfviewer;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                PackageManager.PERMISSION_GRANTED);
+    }
+
+    public void buttonOpenFile(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
+        intent.setType("application/pdf");
+//        intent.setType("*/*");
+        this.startActivity(intent);
+    }
+}
